44bf89f2de2878bacd3fd2f28e66c71cde7ffac9
caio favaro de oliveira <91095945+CaioFavaro@users.noreply.github.com>
2025-01-28 16:35:06 -0300

    Criação da aba sobre
    Aba sobre foi criada.

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,12 +1,31 @@
 import Banner from "../../componentes/Banner";
 import Contato from "../../componentes/Contato";
 import Rodape from "../../componentes/Rodape";
+import styles from './Inicio.module.css';
+import fotoperfil from '../../assets/FotoPerfil.jpeg';
 
 export default function Inicio() {
     return (
         <>
             <Banner> 
             </Banner>
+
+            <div id="problema">
+                <h3 className={styles.subtitulo}>Sobre</h3>
+                <p className={styles.paragrafo}>
+                Olá, tenho 23 anos, atualmente sou formado em Sistemas de informação na FIAP, também atuei estagiando no Departamento e Tecnologia e informação da prefeitura de Cotia, garantindo excelência e perfeccionismo no meu trabalho.
+                </p>
+                <div className={styles.containerProblema}>
+                <ul className={styles.lista}>
+                    <li>Segregação de Clientes</li>
+                    <li>Tempo de Resposta crítico</li>
+                    <li>Degradação de serviço</li>
+                    <li>Bases de Dados segregadas e heterogêneas</li>
+                </ul>
+                <img className={styles.fotoperfil} src={fotoperfil} alt="Problemas identificados" />
+            </div>
+            </div>
+            
             <Contato>
             </Contato>
             <Rodape>

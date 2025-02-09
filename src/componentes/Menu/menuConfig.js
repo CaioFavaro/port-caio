e59f4c14cce61966de4431b73cb89c29f042759a
caio favaro de oliveira <91095945+CaioFavaro@users.noreply.github.com>
2025-02-09 13:57:41 -0300

    mudnaças api 2

--- src/componentes/Menu/menuConfig.js
+++ src/componentes/Menu/menuConfig.js
@@ -8,7 +8,7 @@ export const menuConfig = [
             { to: '/#contato', texto: 'Contato' },
             { 
                 texto: 'Testar API', 
-                acao: () => window.open('http://localhost:3001/api/location', '_blank'),
+                acao: () => window.open('https://port-api-7ef9.onrender.com/api/location', '_blank'),
                 tipo: 'destaque' 
             }
         ]

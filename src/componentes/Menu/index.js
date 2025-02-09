e0e32816d46d65bbf6862c579f1a591e57439eae
caio favaro de oliveira <91095945+CaioFavaro@users.noreply.github.com>
2025-02-09 15:21:39 -0300

    mudnaças api 4

--- src/componentes/Menu/index.js
+++ src/componentes/Menu/index.js
@@ -39,7 +39,7 @@ export default function Menu() {
                     );
                 })}
 
-                {/* Botão de Login */}
+                
                 <div className={styles.loginContainer}>
                     <LoginButton />
                 </div>
